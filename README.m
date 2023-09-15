d2de16b7eb709c32dbf582eb81789b156b5e94dd
Ikhtiyor1993 <145160230+Ikhtiyor1993@users.noreply.github.com>
2023-09-16 00:50:52 +0500

    Ikhtiyor.1993

---
+++ README.m
@@ -0,0 +1,10 @@
+- 👋 H I’m @Ikhtiyor1993
+- 👀 I’m interested in ... Trading
+- 🌱 I’m currently learning ... Crypto Trading
+- 💞️ I’m looking to collaborate on ... Airdrop
+- 📫 How to reach me bybitsafarov@gmail.com
+
+<!---
+Ikhtiyor1993/Ikhtiyor1993 is a ✨ special ✨ repository because its `README.md` (this file) appears on your GitHub profile.
+You can click the Preview link to take a look at your changes.
+--->
